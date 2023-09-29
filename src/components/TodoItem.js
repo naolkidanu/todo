2926fb39018e6d8ae69b6f5e742183a2a86aa44e
Naol Kidanu <sabelkidanu18@gmail.com>
2023-09-29 14:09:54 +0300

    add items

---
+++ src/components/TodoItem.js
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import styles from "../styles/TodoItem.module.css";
+
+const TodoItem = ({ itemProp, handleChange, delTodo }) => {
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  };
+  return (
+    <li className={styles.item}>
+      <div className={styles.content}>
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+        />
+        <span style={itemProp.completed ? completedStyle : null}>
+          {itemProp.title}
+        </span>
+        <button type="submit" onClick={() => delTodo(itemProp.id)}>
+          Delete
+        </button>
+      </div>
+    </li>
+  );
+};
+export default TodoItem;
